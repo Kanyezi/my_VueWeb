ac874c075f1fc2dc8754a58410847ed74d6b332f
GaiShen <1214241948@qq.com>
2025-09-25 17:42:01 +0800

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   env.d.ts 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/main.ts 	new file:   src/nav.vue 	new file:   src/navs/home.vue 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app">
+        <div class="nav"></div>
+        <div class="main">
+            <div class="top-Search">
+
+            </div>
+            <div class="title">
+                <div class="page-title">
+
+                </div>
+                <div class="page-subtitl">
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'app'
+    }
+</script>
+
+<style>
+    .app{
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
