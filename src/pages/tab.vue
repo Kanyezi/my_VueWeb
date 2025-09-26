9d623fcf6cae3fbd3895e843863ca7369d712dfd
乞丐 <1214241948@qq.com>
2025-09-26 16:09:39 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/pages/tab.vue
    	new file:   stores/config.ts

---
+++ src/pages/tab.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="kuang">
+        <div class="title">
+
+        </div>
+        <div class="tabs">
+            <div class="tab"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .tabs{
+        display: flex;
+        flex-direction: row;
+    }
+    .tab{
+        width: 100px;
+        height: 100px;
+        box-shadow: 0 0 10px #ddd;
+    }
+</style>
