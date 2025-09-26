a5b2d9d53b110978ffa10f4ab3dd500440f0f3d9
乞丐 <1214241948@qq.com>
2025-09-26 16:14:51 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   public/user/navigation.json
    	modified:   stores/config.ts

--- stores/config.ts
+++ stores/config.ts
@@ -1,3 +1,14 @@
 import { defineStore } from 'pinia'
 import { ref, reactive } from 'vue'
 
+interface NavItem{
+    id:string
+    name:string
+    icon:string
+    show?:boolean
+}
+
+interface PageDate{
+    title:string
+    subtitle:string
+}
